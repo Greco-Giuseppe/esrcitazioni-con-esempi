019a8c6c72497e89dd1af786a17d750fefadd3ab
Greco-Giuseppe <greco.giuseppe@iisgalvanimi.edu.it>
2022-10-20 10:35:25 +0000

    versionerecente

--- es_main2.py
+++ es_main2.py
@@ -1,3 +1,20 @@
 #per fare una lista , sempre creare una variabile , ma : utilizzare [] e tra gli elementi della lista metto le virgole 
 mialista = [ "bianco" , "verde" , "giallo" ]
+print(mialista)
+#nella lista gli elementi possono essere ripetuti e verranno ristampati tutti indifferenzemente dall'ugualianza tra più elementi
+
+# la funzione len() è utile per contare gli elementi all'interno dell'insieme 
+mialista = [ "bianco" , "verde" , "giallo" ]
+print(len(mialista))
+#RICORDA DI FARE TUTTO ALLINEATO (per evitare eventuali errori)
+
+#le liste possono contenere diversi tipidi variabili 
+
+#per identificare cos'è    
+mialista = [ "bianco" , "verde" , "giallo" ]
 print(type(mialista))
+
+#costruttori diversi , per scrivere in modo diverso la lista , list() - RICORDARSI LE DOPPIE TONDE
+mialista = list (("bianco" , "verde" , "giallo"))
+print(mialista)
+
