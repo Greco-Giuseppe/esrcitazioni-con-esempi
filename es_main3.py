475adea1820daf7c4c11f63fbc14169c8e4de33b
Greco-Giuseppe <greco.giuseppe@iisgalvanimi.edu.it>
2022-10-20 10:51:35 +0000

    nuovaversione

---
+++ es_main3.py
@@ -0,0 +1,29 @@
+# if , else
+
+#condizioni matematiche
+# Equals: a == b
+# Not Equals: a != b
+# Less than: a < b
+# Less than or equal to: a <= b
+# Greater than: a > b
+# Greater than or equal to: a >= b
+
+a = 33
+b = 200
+
+if b > a:
+  print("b is greater than a")
+# SE 'PRINT' VIENE MESSO IN CORRISPONDENZA DI 'IF' VERRA' SEGNALATO UN ERRORE 
+
+# elif
+#per aggiungere una condizione nel caso in cui non sia veriteria la precedente 'elif'
+a = 33
+b = 200
+
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+
+
+
