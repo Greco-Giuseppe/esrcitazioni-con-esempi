b124ec4f971207c37ce82efa82311adbf5042f11
Greco-Giuseppe <greco.giuseppe@iisgalvanimi.edu.it>
2022-11-01 13:15:49 +0000

    finito

---
+++ prep_J.py
@@ -0,0 +1,9 @@
+#Riempi una lista di 10 numeri casuali (utilizza un ciclo for) e stampa solo i numeri pari
+import random
+lista = [random.randrange(1,10) , random.randrange(1,10)  , random.randrange(1,10) , random.randrange(1,10) , random.randrange(1,10) , random.randrange(1,10) , random.randrange(1,10) , random.randrange(1,10) , random.randrange(1,10) ,  random.randrange(1,10) ]
+for x in lista :
+    if x/2 = int 
+        print (X)
+    else : 
+        continue
+        
