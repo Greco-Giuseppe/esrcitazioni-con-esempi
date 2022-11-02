e9be14985d51ad7d537d2418702444e767552d7b
Greco-Giuseppe <greco.giuseppe@iisgalvanimi.edu.it>
2022-11-02 15:15:14 +0000

    esercizioB

--- prep_C.py
+++ prep_C.py
@@ -1,10 +1,10 @@
 #Riempi tre variabili (x, y ,z) con dei valori. Se x è maggiore di y e maggiore di z stampa ‘X è il numero maggiore’ 
 
-x= 223
-y= 30
-z= 22
+x = 13 
+y = 13
+z = 11
 
-if x>y and x>z :
+if x > y and x > z :
     print ("x è il numero maggiore")
 
 
