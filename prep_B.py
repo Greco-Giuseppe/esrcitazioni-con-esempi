933547c5a3b3a91aeca31325d0764e34b1de86e4
Greco-Giuseppe <greco.giuseppe@iisgalvanimi.edu.it>
2022-11-02 15:12:19 +0000

    esercizioB

--- prep_B.py
+++ prep_B.py
@@ -1,7 +1,8 @@
 #Riempi due variabili (x e y) con dei valori. Se x è maggiore di y stampa “ciao”. Se x è minore o uguale a y stampa “arrivederci”
-x = 5
-y = 9
-if x>y : 
+x = 13
+y = 13
+
+if x > y :
     print ("ciao")
-else :
+else : 
     print ("arrivederci")
