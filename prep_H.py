63c2fc6b233aa2947f1ef92471101c5ed5d77d5b
Greco-Giuseppe <greco.giuseppe@iisgalvanimi.edu.it>
2022-11-02 18:04:44 +0000

    ancheH

--- prep_H.py
+++ prep_H.py
@@ -1,6 +1,4 @@
 # Riempi una lista con 5 nomi di Animali e stampa la lista in ordine inverso
 lista = [ "tigre" , "leone" , "pavone" , "tartaruga" , "falco" ]
-for x in lista :
-    for x in range (4 ,0)
-        print (x)
-    
+lista.reverse()
+print (lista)
