7f4a7570db7e7ad387d87f572fa928690cad61f7
Greco-Giuseppe <greco.giuseppe@iisgalvanimi.edu.it>
2022-11-02 18:13:56 +0000

    finito

--- prep_i.py
+++ prep_i.py
@@ -1,6 +1,12 @@
+#Dichiara due liste. Una con nomi di animali e l’altra con numeri. Aggiungi tutti gli elementi della seconda lista alla prima.
+#Stampa soltanto i numeri maggiori di 5
+
+
 animali = ["lucertola","cane","gatto","pipistrello","farfalla"]
-numeri = [1 , 2 , 3 , 4 , 5 ]
+numeri = [35 , 41 , 56 , 4 , 2 ]
+z = animali + numeri 
+print (z)
 for x in numeri :
-    for y in animali :
-        print (x , y)
+    if x > 5 :
+        print (x)
         
