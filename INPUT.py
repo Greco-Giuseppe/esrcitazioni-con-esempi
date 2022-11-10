a96ed4665510d7b3440ac47c48e56a75d360777e
Greco-Giuseppe <greco.giuseppe@iisgalvanimi.edu.it>
2022-11-10 11:41:31 +0000

    10-11

---
+++ INPUT.py
@@ -0,0 +1,8 @@
+#INPUT
+#per un input da tastiera 
+username = input("Enter username:")
+print("Username is: " + username)
+#è possibile utilizzare anche "raw_input" a seconda della versione di python
+username = raw_input("Enter username:")
+print("Username is: " + username)
+#in questo caso raw_input 
---
+++ funzione2.py
@@ -0,0 +1,45 @@
+#FUNZIONI 
+
+#creare una funzione 
+# utilizzare "def"
+def my_function():
+  print("Hello from a function")
+#stampare gli elementi della funzione 
+my_function()
+
+# argomenti 
+# creo un avariabile ("fname" , POSSO CHIAMARLA IN QUALSIASI MODO ) che conterrà le informazioni 
+def my_function(fname):
+  print(fname + " Refsnes")
+
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus")
+
+# creare due argomenti e assegnargli diversi nomi 
+def my_function(fname, lname):
+  print(fname + " " + lname)
+
+my_function("Emil", "Refsnes")
+# se immetto due argomenti ma ne assegno solo uno mi da errore 
+def my_function(fname, lname):
+  print(fname + " " + lname)
+
+my_function("Emil")  #ERRORE 
+
+# utilizzo "*" 
+def my_function(*kids):
+  print("The youngest child is " + kids[2])
+
+my_function("Emil", "Tobias", "Linus")
+#il numero tra le parentesi quadre corrisponde al numero dell'elemnto partendo da 0
+
+#assegno ad ogni argomento un elemento 
+def my_function(child3, child2, child1):
+  print("The youngest child is " + child3)
+
+my_function(child1 = "Emil", child2 = "Tobias", child3 = "Linus")
+#notare come assegnando ad ogni argomernto un nome non necessariamente bisogni utilizzare gli ordini degli argomenti 
+
+
+
