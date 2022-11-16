9d37dad6b5bbfa47a15045265765f8f1a3b0005c
Greco-Giuseppe <greco.giuseppe@iisgalvanimi.edu.it>
2022-11-16 11:44:01 +0000

    AB

---
+++ zINPUT.py
@@ -0,0 +1,4 @@
+#INPUT
+#per un input da tastiera 
+username = input("Enter username:")
+print("Username is: " + username)
---
+++ zprepA-verificafunz.py
@@ -0,0 +1,16 @@
+#A  Chiedi all’utente di inserire 5 numeri e stampa i numeri man mano che vengono inseriti
+numeri = input("inserire un numero:" )
+print("il numero è: " + numeri)
+numeri = input("inserire un numero:" )
+print("il numero è: " + numeri)
+numeri = input("inserire un numero:" )
+print("il numero è: " + numeri)
+numeri = input("inserire un numero:" )
+print("il numero è: " + numeri)
+numeri = input("inserire un numero:" )
+print("il numero è: " + numeri)
+#con il ciclo for 
+for x in range (5) : 
+  numeri = input("inserire un numero:" )
+  print("il numero è: " + numeri )
+
