f3ce7980ef769139b61eefb98ff34e2294723632
Greco-Giuseppe <greco.giuseppe@iisgalvanimi.edu.it>
2022-11-17 11:58:33 +0000

    D

---
+++ zprepD-verificafunz.py
@@ -0,0 +1,8 @@
+#D Chiedi all’utente quanti numeri vuole inserire. Leggi tutti i numeri in input. Stampa tutti i numeri inseriti al quadrato
+
+numeri = int(input("quanti numeri vuoi inserire? : "))
+for a in range (numeri) : 
+    x = int(input("inserisci numero? :"))
+    print (x*x) 
+        
+     
