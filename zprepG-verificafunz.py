67ff87d45cdbf9933835f3a9d93c3a9b0dab8aa9
Greco-Giuseppe <greco.giuseppe@iisgalvanimi.edu.it>
2022-11-30 17:33:29 +0000

    fine

--- zprepG-verificafunz.py
+++ zprepG-verificafunz.py
@@ -1,1 +1,7 @@
 #Definisci una funzione che calcoli in modo ricorsivo la sequenza di Fibonacci di un numero passato come parametro
+
+def fib(n) :
+    if n == 1 : return 1
+    if n == 0 : return 0
+    return fib(n-1) + fib(n-2)
+print(fib(8))
